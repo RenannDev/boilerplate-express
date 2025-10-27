ded540ef4959b211ea20d6157e523d36923aaa08
Vitor Renan .bin <vitorrenan2004@gmail.com>
2025-10-27 13:57:06 -0300

    feat: adicionar middleware de log e rota /now

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
 let express = require('express');
 let app = express();
+let path = require('path')
 require('dotenv').config()
 
 
+app.use(function middleware(req, res, next) {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next();
+});
+
+app.get("/now", function middleware(req, res, next){
+  req.time = new Date().toString()
+  next()
+},(req, res)=>{
+   res.json({time: req.time})
+})
+
 app.get('/', (req, res) => {
-  res.send('Servidor Express rodando! Acesse a rota /json para ver a resposta JSON.');
+  res.sendFile(__dirname + "/views/index.html")
 });
 
 app.get('/json', (req, res) => {
